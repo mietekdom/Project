671fb5a4f564d740c7b12fad2a095cf8c37eb94b
miecz <miecz@LAPTOP-UVM3H6HN>
2022-10-26 09:17:50 +0200

    changes for if date time

--- Project/Program.cs
+++ Project/Program.cs
@@ -4,26 +4,9 @@ namespace Project
 {
     class Program
     {
-        /// <summary>
-        /// stringi and other items 
-        /// </summary>
-        /// <param name="args"></param>
         static void Main(string[] args)
         {
-            string text1 = "some value string";
-            string text2 = "aaaa";
-            string text3 = "bbbbb";
-
-            string tekst = text1 + text2 + text3;
-
-            int a = 7;
-            double b = 3.45;
-
-            string aaa = tekst + " " + a.ToString() + " " + b.ToString();
-
-
-
-            Console.WriteLine(aaa);
+            TestIfandSwitch.TestIfYear();
         }
     }
 }
