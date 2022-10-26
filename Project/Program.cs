b9f74a08ce2c028abbedb303edc9261b98c6a039
miecz <miecz@LAPTOP-UVM3H6HN>
2022-10-26 08:44:19 +0200

    Add project files.

---
+++ Project/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text1 = "some value string";
+            string text2 = "aaaa";
+            string text3 = "bbbbb";
+
+            string tekst = text1 + text2 + text3;
+
+            Console.WriteLine(tekst);
+        }
+    }
+}
